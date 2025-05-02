8cc56dcfc9ec3e4583cf4df188438bdc95061545
ManuNovillo <manuelnovilloinformatica@gmail.com>
2025-05-02 23:04:28 +0200

    added firebase library and firebase_id field to user

--- back-django/social/models/user_model.py
+++ back-django/social/models/user_model.py
@@ -1,9 +1,12 @@
 from . import File
 from django.db import models
 
+
 class User(models.Model):
     email = models.EmailField(unique=True)
     nickname = models.CharField(max_length=30, unique=True)
     profile_picture = models.ForeignKey(File, on_delete=models.PROTECT)
+    firebase_id = models.IntegerField(unique=True)
+
     class Meta:
         db_table = 'user'
