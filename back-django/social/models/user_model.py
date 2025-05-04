4da755ae3cb3d227f1be1c64f5f6af216d53ca22
ManuNovillo <manuelnovilloinformatica@gmail.com>
2025-05-04 21:10:26 +0200

    fixed an error with .env file

--- back-django/social/models/user_model.py
+++ back-django/social/models/user_model.py
@@ -6,6 +6,7 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     nickname = models.CharField(max_length=30, unique=True)
     profile_picture = models.ForeignKey(File, on_delete=models.PROTECT)
+    deleted = models.BooleanField(default=False)
     firebase_id = models.IntegerField(unique=True)
 
     class Meta:
--- back-django/tfg/settings.py
+++ back-django/tfg/settings.py
@@ -4,6 +4,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+print(f"BASE_DIR: {BASE_DIR}")
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
