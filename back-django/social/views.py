9d036ec7ad081de679ae7a8de1777d51d10382bf
Manuel Novillo Romocea <manuel.novillo@FIBRITEL.local>
2025-05-12 12:30:54 +0200

    added token verification

--- back-django/social/views.py
+++ back-django/social/views.py
@@ -2,19 +2,49 @@ from django.http import JsonResponse
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_http_methods
+from functools import wraps
 
-import 
+import jwt
+from jwt import InvalidTokenError
 
+import os
+
+
+SECRET_KEY = os.getenv('SECRET_KEY_SUPABASE')
+ALGORITHM = "HS256"
+AUDIENCE = "authenticated"
+
+def verify_supabase_token(token):
+    if SECRET_KEY is None:
+        return None
+    try:
+        decoded = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM],
+            audience=AUDIENCE
+        )
+        return decoded  # contiene el sub (id del usuario), email, etc.
+    except InvalidTokenError as e:
+        print("Token inválido:", str(e))
+        return None
 from social.serializers import PostSerializer, UserSerializer, CommentSerializer
 from social.models import Post, User, Comment
 
 
-def validate_token(endpoint):
+
+def require_supabase_token(view_func):
+    @wraps(view_func)
     def wrapper(request, *args, **kwargs):
-        token = request.GET.get('token') or request.POST.get('token')
-        if not token:
-            return JsonResponse({'error': 'Token is missing'}, status=401)
-        decoded = jwt.decode(token, 'your_secret_key', algorithms=['HS256'])
+        auth = request.headers.get('Authorization', '')
+        if not auth.startswith('Bearer '):
+            return JsonResponse({'error': 'Token no encontrado'}, status=401)
+        token = auth.split(' ')[1]
+        payload = verify_supabase_token(token)
+        if not payload:
+            return JsonResponse({'error': 'Token inválido'}, status=401)
+        request.user_payload = payload
+        return view_func(request, *args, **kwargs)
     return wrapper
 
 
@@ -68,6 +98,7 @@ def get_post_comments(request, post_id):
 
 
 @require_http_methods(["POST"])
+@require_supabase_token
 def upload_post(request):
     serializer = PostSerializer(data=request.POST)
     if serializer.is_valid():
@@ -77,6 +108,7 @@ def upload_post(request):
 
 
 @require_http_methods(["PUT"])
+@require_supabase_token
 def update_user_profile(request):
     user = get_object_or_404(User, id=request.POST.get('id'), deleted=False)
     serializer = UserSerializer(user, data=request.POST, partial=True)
@@ -86,8 +118,8 @@ def update_user_profile(request):
     return JsonResponse(serializer.errors, status=400)
 
 
-@validate_token
 @require_http_methods(["POST"])
+@require_supabase_token
 def upload_comment(request):
     serializer = CommentSerializer(data=request.POST)
     if serializer.is_valid():
